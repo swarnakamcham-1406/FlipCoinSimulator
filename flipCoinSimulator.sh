0ee6ad77674db9668e77cd371f448c7e32dccdfa
kamchamswarna <kamchamswarna@gmail.com>
2020-10-14 14:22:49 +0530

    flipping a coin to display heads or tails as winner

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,1 +1,17 @@
+#! /bin/bash -x
 echo "welcome to the flipCoinSimulator"
+
+head=0
+tail=1
+
+flipCoin=$((RANDOM%2))
+
+if [ $flipCoin -eq $head ]
+then
+    echo "HEAD"
+    echo "You Win"
+else
+    echo "TAIL"
+   echo "you Win"
+fi
+
