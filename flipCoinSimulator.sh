cfb753d04c9096f81221cc726bc00ca709b4c54e
kamchamswarna <kamchamswarna@gmail.com>
2020-10-14 14:37:25 +0530

    multiple times of flipping a coin and show number of times head and tail

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,17 +1,26 @@
 #! /bin/bash -x
 echo "welcome to the flipCoinSimulator"
 
-head=0
-tail=1
+echo "number of times flipping a coin"
 
+read n
+flip=1
 flipCoin=$((RANDOM%2))
+h=0
+t=0
+while [ $flip -le $n ]
+do
+flip=$(($flip + 1))
 
-if [ $flipCoin -eq $head ]
+if [ $flipCoin -eq 0 ]
 then
     echo "HEAD"
-    echo "You Win"
+     h=$(($h+1))
+echo "$h"
 else
     echo "TAIL"
-   echo "you Win"
+    t=$(($t+1))
+echo "$t"
 fi
+done
 
